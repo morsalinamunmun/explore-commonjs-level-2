bbc8cdeec36ca96864c8caf11dd2d766913276cb
morsalinamunmun <morsalinamunmun@gmail.com>
2025-06-13 14:14:21 +0600

    read write sream function create

--- Module-13/fs.js
+++ Module-13/fs.js
@@ -41,3 +41,37 @@ console.log(text, 'last show result');
 
 console.log(text);
 console.log("Task 3");
+
+const readStream = fs.createReadStream("./hello-world.txt", {encoding : "utf-8"})
+const writeStream = fs.createWriteStream("./hello.txt", {encoding : "utf-8"})
+
+readStream.on("data", (data) => {
+  console.log(data);
+
+  writeStream.write(data, (err) => {
+    if(err){
+      throw Error("Error", err)
+    }
+  })
+})
+
+readStream.on("error", (err) => {
+  if (err) {
+    throw Error("Error", err);
+  }
+})
+
+writeStream.on("error", (err) => {
+  if (err) {
+    throw Error("Error", err);
+  }
+});
+
+readStream.on("end", () => {
+  console.log("reading ended");
+  writeStream.end()
+})
+
+writeStream.on("finish", () => {
+  console.log("Written successfuly");
+})
